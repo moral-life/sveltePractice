1702ba2740d9690edf5461a1ae3acf7f2efbc3fb
Moral_Life <leeminam32@gmail.com>
2023-03-15 23:29:18 +0900

    counter page add reset button
    reset_zero 함수

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
-<div>
-    <a href="/">main page</a>
-    <a href="/about">about page</a>
-    <a href="/counter">counter page</a>
-</div>
+<header>
+    <div>
+        <a href="/counter">counter page</a>
+        <a href="/">main page</a>
+        <a href="/about">about page</a>
+    </div>
+</header>
 
 <slot></slot>
+
+<style lang="scss">
+    header {
+        background-color: navy;
+        height: 50px;
+    }
+
+</style>
--- src/routes/counter/+page.svelte
+++ src/routes/counter/+page.svelte
@@ -6,8 +6,12 @@
     function minus() {
         num -= 1;
     }
+    function reset_zero() {
+        num = 0;
+    }
 </script>
 <h1>this is Counter Page</h1>
 <div>now num is {num}</div>
 <button on:click={plus}>+</button>
 <button on:click={minus}>-</button>
+<button on:click={reset_zero}>reset</button>
